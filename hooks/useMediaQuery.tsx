8ba35eec8d7875b22fcbe5d19dd9f81bf8ff1d7a
awebcode <asikurrahaman997@gmail.com>
2024-10-15 23:27:14 +0600

    updated

--- hooks/useMediaQuery.tsx
+++ hooks/useMediaQuery.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 
 const useMediaQuery = (query: string): boolean => {
-  const [matches, setMatches] = useState<boolean>(() => window.matchMedia(query).matches);
+    const [matches, setMatches] = useState<boolean>(() => {
+      if( typeof window!==undefined)
+          return window.matchMedia(query).matches;
 
-  useEffect(() => {
-    const mediaQueryList = window.matchMedia(query);
+      return false;
+    });
 
-    const handleChange = (event: MediaQueryListEvent) => {
-      setMatches(event.matches);
+    useEffect(() => {
+      const mediaQueryList = window.matchMedia(query);
+
+      const handleChange = (event: MediaQueryListEvent) => {
+        setMatches(event.matches);
     };
 
     mediaQueryList.addEventListener("change", handleChange);
